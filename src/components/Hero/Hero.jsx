8d10490c0a9563bb90f896a892375618d3871202
eliasgiannantonio <giannantonioelias@gmail.com>
2024-08-02 19:09:23 -0300

    Diseño responsive

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.png'
+import { Link } from 'react-scroll'
 
 function Hero() {
   return (
@@ -9,7 +10,11 @@ function Hero() {
         <h1><span>Soy Elias Giannantonio,</span> desarrollador web.</h1>
         <p>Desarrollador web apasionado y creativo en busca de nuevos desafíos. Bienvenido a mi portfolio.</p>
         <div className="hero-action">
-            <div className="hero-connect">Conecta conmigo</div>
+            <div className="hero-connect">
+              <Link to='contacto' smooth={true} spy={true} offset={-20} duration={500} className='anchor-link' href='#contacto'>
+                <p onClick={() => setMenu("contacto")}>Conecta conmigo</p>
+              </Link>
+            </div>
             <div className="hero-curriculum">Mi currículum</div>
         </div>
     </div>
