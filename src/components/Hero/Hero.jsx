54aec0d0b5bec4fc3cabd434a592998523405fc0
eliasgiannantonio <giannantonioelias@gmail.com>
2024-08-03 13:33:38 -0300

    Agrego mi cv!

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,17 +6,19 @@ import { Link } from 'react-scroll'
 function Hero() {
   return (
     <div id='inicio' className='hero'>
-        <img src={profile_img} alt="" />
-        <h1><span>Soy Elias Giannantonio,</span> desarrollador web.</h1>
-        <p>Desarrollador web apasionado y creativo en busca de nuevos desafíos. Bienvenido a mi portfolio.</p>
-        <div className="hero-action">
-            <div className="hero-connect">
-              <Link to='contacto' smooth={true} spy={true} offset={-20} duration={500} className='anchor-link' href='#contacto'>
-                <p onClick={() => setMenu("contacto")}>Conecta conmigo</p>
-              </Link>
-            </div>
-            <div className="hero-curriculum">Mi currículum</div>
-        </div>
+      <img src={profile_img} alt="" />
+      <h1><span>Soy Elias Giannantonio,</span> desarrollador web.</h1>
+      <p>Desarrollador web apasionado y creativo en busca de nuevos desafíos. Bienvenido a mi portfolio.</p>
+      <div className="hero-action">
+        <Link to='contacto' smooth={true} spy={true} offset={-20} duration={500} className='hero-connect-link' href='#contacto'>
+          <div className="hero-connect">
+            <p onClick={() => setMenu("contacto")}>Conecta conmigo</p>
+          </div>
+        </Link>
+        <a className="hero-curriculum-link" target="_blank" href='https://drive.google.com/file/d/1PrZV9HPoN9FbOtwdAqolhP9eukdoiZJ3/view?usp=sharing'>
+          <div className="hero-curriculum">Mi currículum</div>
+        </a>
+      </div>
     </div>
   )
 }
