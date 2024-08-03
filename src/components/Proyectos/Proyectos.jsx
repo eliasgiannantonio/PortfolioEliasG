0ff7dfc63ed9b38d9715975bed38d4507b578080
eliasgiannantonio <giannantonioelias@gmail.com>
2024-08-03 13:20:29 -0300

    Ultimos ajustes! Agrego mi ultimo proyecto de mi todo list

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -10,7 +10,7 @@ const Proyectos = () => {
         </div>
         <div className="proyectos-container">
             {mywork_data.map((work, index) => {
-                return <img key={index} src={work.w_img} alt="" />
+                return <a target="blank" href={work.w_url}><img key={index} src={work.w_img} alt="" /></a>
             })}
         </div>
         <div className="proyectos-ver-mas">
