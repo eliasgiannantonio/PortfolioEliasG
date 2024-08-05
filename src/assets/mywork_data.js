b9817bd483ce5eea11a560554f0832aa5e29a84f
eliasgiannantonio <giannantonioelias@gmail.com>
2024-08-05 19:59:28 -0300

    Subo proyecto ClimaApp

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,10 +1,17 @@
 import project1_img from '../assets/project_1.png'
+import project2_img from '../assets/project_2.png'
 
 const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
         w_img:project1_img,
+        w_url: "https://clima-app-eg.netlify.app/"
+    },
+    {
+        w_no:2,
+        w_name:"Web design",
+        w_img:project2_img,
         w_url: "https://todo-eg-app.netlify.app/"
     },
 ]
