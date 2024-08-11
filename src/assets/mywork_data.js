beb0de4b3d94a55c628bbafec479c195570e83fa
eliasgiannantonio <giannantonioelias@gmail.com>
2024-08-11 12:28:51 -0300

    Actualizo portfolio, subo proyecto runthecode (e-commerce zapatillas)

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,17 +1,24 @@
 import project1_img from '../assets/project_1.png'
 import project2_img from '../assets/project_2.png'
+import project3_img from '../assets/project_3.png'
 
 const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
         w_img:project1_img,
-        w_url: "https://clima-app-eg.netlify.app/"
+        w_url: "https://runthecode.netlify.app/"
     },
     {
         w_no:2,
         w_name:"Web design",
         w_img:project2_img,
+        w_url: "https://clima-app-eg.netlify.app/"
+    },
+    {
+        w_no:3,
+        w_name:"Web design",
+        w_img:project3_img,
         w_url: "https://todo-eg-app.netlify.app/"
     },
 ]
